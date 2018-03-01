65c97acbca50ec17b9f068c84eb1e9ba8459d898
Alejok28 <alejo_k28@hotmail.com>
2018-03-01 16:24:04 -0500

    Counts all characters and shows in the label

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    }
+  }
+  updateValue(e) {
+    this.setState({
+      value: e.target.value.length
+    })
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.updateValue.bind(this)}></textarea>
+        <div className="counter">{this.state.value}</div>
       </div>
     );
   }
